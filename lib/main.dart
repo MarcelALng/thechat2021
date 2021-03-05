1ca36c8bb4c6349707fd987ba6d2b47a01978160
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-03-05 12:45:18 +0100

    [Constants] Add routes constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'constants/routes_constant.dart';
+
 void main() => TheChatApp();
 
 class TheChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: ,
+      routes:  ,
+    );
   }
 }
