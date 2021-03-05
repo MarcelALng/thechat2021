5ce681b097ec2cbab01c40e343b1abd2c2e113f7
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-03-05 13:47:20 +0100

    [LIB] add constants and screens folders

---
+++ lib/constants/routes_constant.dart
@@ -0,0 +1,10 @@
+import 'package:thechat2021/screens/welcome_screen.dart';
+
+class RoutesConstant {
+  static const String welcome = '/';
+  static const String register = '/register';
+  static const String signing = '/signin';
+  static const String usermain = '/usermain';
+  static const String userchat = '/chat';
+  static const String changePassword = '/changepassword';
+}
