bec348a72e1811bb4e8dad067f10b33b183dd61f
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-03-08 08:29:06 +0100

    Create app and logo works

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'constants/routes_constant.dart';
+import 'screens/welcome_screen.dart';
 
-void main() => TheChatApp();
+void main() => runApp(TheChatApp());
 
 class TheChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: ,
-      routes:  ,
+      initialRoute: RoutesConstant.welcome,
+      routes: {
+        RoutesConstant.welcome: (context) => WelcomeScreen(),
+        // RoutesConstant.register: (context) => RegisterScreen(),
+        // RoutesConstant.signing: (context) => SigninScreen(),
+        // RoutesConstant.userchat: (context) => UserChatScreen(),
+        // RoutesConstant.usermain: (context) => UserMainScree(),
+        // RoutesConstant.changePassword: (context) => ChangePassword(),
+      },
     );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,10 +1,28 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-class Welcome extends StatelessWidget {
+class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      backgroundColor: Color(0xff4682B4),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: Image.asset("assets/logo.png"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
