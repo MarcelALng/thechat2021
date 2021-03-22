6b927fe095ea7538e10f0938c100bcb398653439
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-03-22 18:42:48 +0100

    [Firebase] Collection test created OK, show from user home screen OK

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:thechat2021/constants/routes_constant.dart';
 
 class ChatController {
   final FirebaseAuth _firebase = FirebaseAuth.instance;
+  final Firestore _cloudFirestore = Firestore.instance;
   FirebaseUser _activeUser;
+
+  Future<QuerySnapshot> getDiscussions() {
+    return _cloudFirestore.collection("discussions").getDocuments();
+  }
 
   Future<FirebaseUser> checkIdentity(BuildContext context) async {
     final FirebaseUser _user = await _firebase.currentUser();
--- lib/screens/user_home_screen.dart
+++ lib/screens/user_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'components/appbar_component.dart';
@@ -8,6 +9,7 @@ import 'package:thechat2021/constants/global_constant.dart';
 import 'package:thechat2021/constants/routes_constant.dart';
 
 class UserHomeScreen extends StatelessWidget {
+  final _controller = ChatController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,20 +23,36 @@ class UserHomeScreen extends StatelessWidget {
             color: Colors.red,
           ),
           onPressed: () {
-            ChatController().signOut();
+            _controller.signOut();
             Navigator.pushReplacementNamed(context, RoutesConstant.welcome);
           },
         ),
       ]).build(),
       body: SafeArea(
         child: FutureBuilder(
-          future: ChatController().checkIdentity(context),
+          future: _controller.checkIdentity(context),
           builder: (context, AsyncSnapshot<FirebaseUser> data) {
             if (!data.hasData) {
               return CircularProgressIndicator();
             }
-            return Container(
-              child: Text("Cela marche!"),
+            return FutureBuilder(
+              future: _controller.getDiscussions(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> discussions) {
+                if (!discussions.hasData) {
+                  return CircularProgressIndicator();
+                }
+                return ListView.builder(
+                  itemCount: discussions.data.documents.length,
+                  itemBuilder: (context, item) {
+                    return Container(
+                      color: Colors.white,
+                      child: ListTile(
+                        title: Text(discussions.data.documents[item]["name"]),
+                      ),
+                    );
+                  },
+                );
+              },
             );
           },
         ),
